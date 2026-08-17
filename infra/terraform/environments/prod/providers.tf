ec6c2b7061f2abf7f00cfbb45a47debea245e083
shyam-medh <baranwal07shyam@gmail.com>
2026-08-18 00:40:33 +0530

    Complete Stage 2: Terraform State Bootstrap

--- infra/terraform/environments/prod/providers.tf
+++ infra/terraform/environments/prod/providers.tf
@@ -4,7 +4,7 @@ terraform {
   # Run the code in `infra/terraform/bootstrap` first to generate your unique bucket name,
   # then replace `YOUR_UNIQUE_BUCKET_NAME_HERE` below with the output.
   backend "s3" {
-    bucket         = "YOUR_UNIQUE_BUCKET_NAME_HERE"
+    bucket         = "django-notes-app-terraform-state-215f80f7"
     key            = "prod/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "django-notes-app-terraform-locks"
