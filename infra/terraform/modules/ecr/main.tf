70805899da01dfa599c876b100d27c16193796a3
shyam-medh <baranwal07shyam@gmail.com>
2026-08-18 02:56:47 +0530

    refactor: remove ecr_nginx and infra/nginx/ - V2 uses S3+CloudFront for React, no Nginx needed

--- infra/terraform/modules/ecr/main.tf
+++ infra/terraform/modules/ecr/main.tf
@@ -3,30 +3,8 @@ module "ecr_django" {
   version = "~> 2.0"
 
   repository_name = "django-notes-backend"
-  
-  repository_lifecycle_policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1,
-        description  = "Keep last 30 images",
-        selection = {
-          tagStatus     = "any",
-          countType     = "imageCountMoreThan",
-          countNumber   = 30
-        },
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
-}
 
-module "ecr_nginx" {
-  source  = "terraform-aws-modules/ecr/aws"
-  version = "~> 2.0"
-
-  repository_name = "django-notes-nginx"
+  repository_image_tag_mutability = "IMMUTABLE"
 
   repository_lifecycle_policy = jsonencode({
     rules = [
